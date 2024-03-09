9a0e40d8cc9f99d00c46df0ce11282b2552eb8b2
rmnsainadh <ravinenisainadh@gmail.com>
2024-03-09 12:50:35 +0530

    login and reg done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,17 @@
-function App() {
-  return <div className="App"></div>;
-}
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import RegisterPage from './Pages/Register_Page';
+import LoginPage from './Pages/Login_page';
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<RegisterPage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
+    </Router>
+  );
+};
 
 export default App;
