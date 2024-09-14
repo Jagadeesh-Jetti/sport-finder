5d4b4e591e360a83f770208e1750950684511631
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2024-09-14 22:54:47 +0530

    refactor: fixed exports

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Routes, Route } from "react-router";
 import { HomePage } from "./components/HomePage/HomePage";
 
-function App() {
+export const App = () => {
   return (
     <div className="App">
       <Routes>
@@ -9,4 +9,4 @@ function App() {
       </Routes>
     </div>
   );
-}
+};
