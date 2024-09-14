acaf6219669144f85d55db089f8e987185cc1302
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2024-09-14 22:24:30 +0530

    feat: added routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { Routes, Route } from "react-router";
+import { HomePage } from "./components/HomePage/HomePage";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
