3186a2cbed17eb8f283ceb2d91efa39b578e9b80
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2024-09-14 22:25:15 +0530

    feat: created home page component

---
+++ src/components/HomePage/HomePage.jsx
@@ -0,0 +1,12 @@
+export const HomePage = () => {
+  return (
+    <div className="homepage_main">
+      <div className="homepage_navbar">
+        <div className="homepage_navbar_child">logo</div>
+        <div className="homepage_navbar_child">book</div>
+        <div className="homepage_navbar_child">play</div>
+        <div className="homepage_navbar_child"></div>
+      </div>
+    </div>
+  );
+};
